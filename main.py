8be4b0123afe443f80a153e61e652641ce43ed4b
aryehlev <59808808+aryehlev@users.noreply.github.com>
2021-07-07 11:54:52 +0300

    pandas solution

--- main.py
+++ main.py
@@ -2,27 +2,31 @@ from fastapi import FastAPI
 
 from data_handler import data_handler
 
-
+dh = data_handler()
 app = FastAPI()
 
 @app.on_event("startup")
 def startup_event():
-    dh = data_handler()
-
-    dh.setup_session_df()
+    dh.setup_df()
+  
 
 @app.get("/keepalive")
 async def root():
     return {"ready": True, "message": "waiting for query"}
 
 
-@app.get("/userStats")
-async def root():
-    import asyncio
-    await asyncio.sleep(100000)
+@app.get("/userStats/{user_id}")
+async def root(user_id):
+    
+    data = dh.get_user_info(user_id)
+    result = await data
+    return result
 
-    return "hello"
+@app.get("/sessionId/{session_id}")
+async def root(session_id):
+    
+    data =  await dh.get_session_info(session_id)
+   
+    return data
 
-@app.get("/sessionId")
-async def root():
-    return {"ready": True, "message": "waiting for query"}
+
